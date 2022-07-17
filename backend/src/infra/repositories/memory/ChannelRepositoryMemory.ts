c75aa8743a898250cd6e0b41aa1966a94064cd29
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-07-17 18:36:30 -0300

    REFACTOR: backend - Channel e Playlist repositories in memory

--- backend/src/infra/repositories/memory/ChannelRepositoryMemory.ts
+++ backend/src/infra/repositories/memory/ChannelRepositoryMemory.ts
@@ -1,39 +1,25 @@
-import { ChannelDTO } from '../../../domain/dto/ChannelDTO';
-import { Channel, ChannelAttributes } from '../../../domain/entities/Channel';
+import { ChannelAttributes } from '../../../domain/entities/Channel';
 import { ChannelRepository } from '../../../domain/repositories/ChannelRepository';
 import { MemoryDatabase } from '../../../test/MemoryDatabase';
 
 export class ChannelRepositoryMemory implements ChannelRepository {
   constructor(private memoryDatabase: MemoryDatabase) {}
-  async findAll(): Promise<ChannelDTO[] | undefined> {
-    const channels = this.memoryDatabase.channels.map((channel) => {
-      const channelDTO: ChannelDTO = {
-        id: channel.id,
-        ...channel
-      };
-      return channelDTO;
-    });
-
-    return Promise.resolve(channels);
+  async findAll(): Promise<ChannelAttributes[] | undefined> {
+    return Promise.resolve(this.memoryDatabase.channels);
   }
-  async findById(id: string): Promise<ChannelDTO | undefined> {
+  async findById(id: string): Promise<ChannelAttributes | undefined> {
     const channelFound = this.memoryDatabase.channels.find(
       (channel) => channel.id === id
     );
 
     if (!channelFound) return Promise.resolve(undefined);
 
-    const channelDTO: ChannelDTO = {
-      id: channelFound.id,
-      ...channelFound
-    };
-
-    return Promise.resolve(channelDTO);
+    return Promise.resolve(channelFound);
   }
-  async create(channel: Channel): Promise<void> {
+  async create(channel: ChannelAttributes): Promise<void> {
     Promise.resolve(
       this.memoryDatabase.channels.push({
-        ...channel.getAttributes()
+        ...channel
       })
     );
   }
--- backend/src/infra/repositories/memory/PlaylistRepositoryMemory.ts
+++ backend/src/infra/repositories/memory/PlaylistRepositoryMemory.ts
@@ -1,37 +1,25 @@
-import { PlaylistDTO } from '../../../domain/dto/PlaylistDTO';
-import { Playlist } from '../../../domain/entities/Playlist';
+import { PlaylistAttributes } from '../../../domain/entities/Playlist';
+import { VideoInPlaylistAttributes } from '../../../domain/entities/VideoInPlaylist';
 import { PlaylistRepository } from '../../../domain/repositories/PlaylistRepository';
 import { MemoryDatabase } from '../../../test/MemoryDatabase';
 
 export class PlaylistRepositoryMemory implements PlaylistRepository {
   constructor(private memoryDatabase: MemoryDatabase) {}
 
-  async findAll(): Promise<PlaylistDTO[] | undefined> {
-    const playlists = this.memoryDatabase.playlists.map((playlist) => {
-      const playlistDTO: PlaylistDTO = {
-        id: playlist.id,
-        ...playlist
-      };
-      return playlistDTO;
-    });
-    return playlists;
+  async findAll(): Promise<PlaylistAttributes[] | undefined> {
+    return Promise.resolve(this.memoryDatabase.playlists);
   }
-  async findById(id: string): Promise<PlaylistDTO | undefined> {
+  async findById(id: string): Promise<PlaylistAttributes | undefined> {
     const playlistFound = this.memoryDatabase.playlists.find(
       (playlist) => playlist.id === id
     );
 
-    const playlistDTO: PlaylistDTO = {
-      id: playlistFound.id,
-      ...playlistFound
-    };
-
-    return Promise.resolve(playlistDTO);
+    return Promise.resolve(playlistFound);
   }
-  async create(playlist: Playlist): Promise<void> {
+  async create(playlist: PlaylistAttributes): Promise<void> {
     Promise.resolve(
       this.memoryDatabase.playlists.push({
-        ...playlist.getAttributes()
+        ...playlist
       })
     );
   }
