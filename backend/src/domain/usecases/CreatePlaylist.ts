3fe196492edb842e607ce7f2bc42e05339d11144
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-07-17 18:41:34 -0300

    REFACTOR: backend - CreatePlaylist usecase

--- backend/src/domain/usecases/CreatePlaylist.ts
+++ backend/src/domain/usecases/CreatePlaylist.ts
@@ -1,4 +1,4 @@
-import { ChannelNotFoundError } from '../../errors/ChannelNotFoundError';
+import { NotFoundError } from '../../errors/NotFoundError';
 import { Playlist, PlaylistAttributes } from '../entities/Playlist';
 import { IDGenerator } from '../libs/IDGenerator';
 import { Validator } from '../libs/Validator';
@@ -15,20 +15,20 @@ type CreatePlaylistDependencies = {
 export class CreatePlaylist {
   constructor(private dependencies: CreatePlaylistDependencies) {}
 
-  public async execute(playlist: PlaylistAttributes) {
+  public async execute(input: PlaylistAttributes): Promise<void> {
     const channelExists = await this.dependencies.channelRepository.findById(
-      playlist.id_channel
+      input.id_channel
     );
 
     if (!channelExists) {
-      throw new ChannelNotFoundError(playlist.id_channel);
+      throw new NotFoundError(input.id_channel, 'Channel');
     }
 
-    const newPlaylist = new Playlist(playlist, {
+    const playlist = new Playlist(input, {
       idGenerator: this.dependencies.idGenerator,
       validator: this.dependencies.playlistValidator
     });
 
-    await this.dependencies.playlistRepository.create(newPlaylist);
+    await this.dependencies.playlistRepository.create(playlist.getAttributes());
   }
 }
