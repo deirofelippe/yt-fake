77f31581c35268bcd89da6b69c1f9d5cea23f5eb
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-07-17 18:32:16 -0300

    FEAT: backend - adiciona lancamento de erro customizado e formatacao do erro no JoiValidator

--- backend/src/infra/libs/joi/JoiValidator.ts
+++ backend/src/infra/libs/joi/JoiValidator.ts
@@ -1,10 +1,29 @@
-import Joi from 'joi';
+import Joi, { ValidationErrorItem, ValidationError } from 'joi';
 import { Validator } from '../../../domain/libs/Validator';
+import {
+  FieldError,
+  FieldsValidationError
+} from '../../../errors/FieldsValidationError';
 
 export class JoiValidator implements Validator {
   constructor(private schema: Joi.Schema) {}
 
-  execute(object: any): void {
-    Joi.assert(object, this.schema, { abortEarly: false });
+  public execute(object: any): void {
+    try {
+      Joi.assert(object, this.schema, { abortEarly: false });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        const errorMessage = this.formatError(error.details);
+        throw new FieldsValidationError(errorMessage);
+      }
+      throw error;
+    }
+  }
+
+  private formatError(errors: ValidationErrorItem[]): FieldError[] {
+    return errors.map((error) => ({
+      field: error.context.label,
+      message: error.message
+    }));
   }
 }
