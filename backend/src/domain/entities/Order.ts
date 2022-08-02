ba4d590cbfa2903742518263d0b25668330292f6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-02 18:04:36 -0300

    FEAT: backend - cria a entity Order

---
+++ backend/src/domain/entities/Order.ts
@@ -0,0 +1,53 @@
+import { IDGenerator } from '../libs/IDGenerator';
+import { BuyItemInput } from '../usecases/BuyItemUsecase';
+
+export type OrderAttributes = {
+  id: string;
+  id_channel: string;
+  items: OrderItemAttributes[];
+};
+export type OrderItemAttributes = {
+  id: string;
+  id_order: string;
+  id_purchased_item: string;
+  type: 'video' | 'playlist';
+};
+export type OrderDependencies = {
+  idGenerator: IDGenerator;
+};
+
+export class Order {
+  private constructor(private attributes: OrderAttributes) {}
+
+  public static create(dependencies: OrderDependencies, input: BuyItemInput) {
+    const { idGenerator } = dependencies;
+
+    const id_order = idGenerator.generate();
+    const orderItem = input.items.map(
+      (item): OrderItemAttributes => ({
+        id: idGenerator.generate(),
+        id_order,
+        id_purchased_item: item.id,
+        type: item.type
+      })
+    );
+    const order: OrderAttributes = {
+      id: id_order,
+      id_channel: input.id_authenticated_channel,
+      items: orderItem
+    };
+
+    return new Order(order);
+  }
+  get id() {
+    return this.attributes.id;
+  }
+  public getOrder(): Omit<OrderAttributes, 'items'> {
+    const order = JSON.parse(JSON.stringify(this.attributes));
+    delete order.items;
+    return order;
+  }
+  public getOrderItems(): OrderItemAttributes[] {
+    return this.attributes.items;
+  }
+}
