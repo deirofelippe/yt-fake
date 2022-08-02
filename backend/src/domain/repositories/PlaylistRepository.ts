304e3b85d5afcb8343c3a4ea35f60aa71a3985ef
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-02 18:39:01 -0300

    REFACTOR: backend - mudanca no retorno e nomes das interfaces dos repositories de Video e Playlist

--- backend/src/domain/repositories/PlaylistRepository.ts
+++ backend/src/domain/repositories/PlaylistRepository.ts
@@ -1,11 +1,10 @@
-import { FindAllVideosOutput } from '../../infra/repositories/memory/PlaylistRepositoryMemory';
 import { PlaylistAttributes } from '../entities/Playlist';
 import { VideoInPlaylistAttributes } from '../entities/VideoInPlaylist';
 
 export interface PlaylistRepository {
-  findAll(): Promise<PlaylistAttributes[] | undefined>;
+  findAll(): Promise<PlaylistAttributes[] | []>;
   findById(id: string): Promise<PlaylistAttributes | undefined>;
   create(playlist: PlaylistAttributes): Promise<void>;
   addVideo(videoInPlaylist: VideoInPlaylistAttributes): Promise<void>;
-  findAllVideos(id_playlist: string): Promise<FindAllVideosOutput>;
+  findPlaylistsByIds(ids: string): Promise<PlaylistAttributes[] | []>;
 }
--- backend/src/domain/repositories/VideoRepository.ts
+++ backend/src/domain/repositories/VideoRepository.ts
@@ -1,7 +1,17 @@
 import { VideoAttributes } from '../entities/Video';
 
 export interface VideoRepository {
-  findAll(): Promise<VideoAttributes[] | undefined>;
+  findAll(): Promise<VideoAttributes[] | []>;
   findById(id: string): Promise<VideoAttributes | undefined>;
   create(video: VideoAttributes): Promise<void>;
+  findVideosByIds(ids: string): Promise<VideoAttributes[] | []>;
+  findAllVideosByPlaylist(id_playlist: string): Promise<FindAllVideosOutput>;
 }
+
+export type FindAllVideosOutput =
+  | {
+      id: string;
+      title: string;
+      thumbnail: string;
+    }[]
+  | [];
