76b2eb9bf7a1ab5d0ee8bf9ecb2ebfd4abfc00fc
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-02 18:44:42 -0300

    TEST: backend - MemoryDatabase, adiciona atributos de order e orderItem

--- backend/src/test/MemoryDatabase.ts
+++ backend/src/test/MemoryDatabase.ts
@@ -1,9 +1,12 @@
 import { ChannelAttributes } from '../domain/entities/Channel';
+import { OrderAttributes, OrderItemAttributes } from '../domain/entities/Order';
 import { PlaylistAttributes } from '../domain/entities/Playlist';
 import { VideoAttributes } from '../domain/entities/Video';
 import { VideoInPlaylistAttributes } from '../domain/entities/VideoInPlaylist';
 
 export class MemoryDatabase {
+  public orders: Omit<OrderAttributes, 'items'>[] = [];
+  public orderItems: OrderItemAttributes[] = [];
   public channels: ChannelAttributes[] = [];
   public playlists: PlaylistAttributes[] = [];
   public videos: VideoAttributes[] = [];
