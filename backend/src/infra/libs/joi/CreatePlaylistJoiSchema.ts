47f7731afc35d604e2937fd49578d2ecc83b4546
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-05 01:27:34 -0300

    FEAT: backend - infra/joi, adiciona mais validacoes e mensagens de erros no scheme CreatePlaylist

--- backend/src/infra/libs/joi/CreatePlaylistJoiSchema.ts
+++ backend/src/infra/libs/joi/CreatePlaylistJoiSchema.ts
@@ -1,18 +1,23 @@
 import Joi from 'joi';
 import { PlaylistVisibility } from '../../../domain/entities/Playlist';
+import { validationMessages as message } from '../../../domain/libs/ValidationMessages';
 
 export const createPlaylistJoiSchema = Joi.object({
-  id_authenticated_channel: Joi.string().required(),
-  title: Joi.string().required(),
-  price: Joi.number().min(0),
-  visibility: Joi.string().equal(
-    PlaylistVisibility.PUBLIC,
-    PlaylistVisibility.PRIVATE
-  ),
+  id_authenticated_channel: Joi.string().empty().required().max(50),
+  title: Joi.string().empty().required().max(100),
+  price: Joi.number().min(0).default(0),
+  visibility: Joi.string()
+    .equal(PlaylistVisibility.PUBLIC, PlaylistVisibility.PRIVATE)
+    .required()
+    .messages({
+      'any.only': 'Deve ser "publico" ou "privado".'
+    }),
   description: Joi.string()
 }).messages({
-  'any.required': 'Não pode estar vazio',
-  'number.min': 'Deve ser maior que {#limit} ou {#limit}',
-  'any.only': 'Deve ser "publico" ou "privado"',
-  'string.base': 'Deve conter letras'
+  'any.required': message.required,
+  'number.min': 'Deve ser maior que {#limit} ou {#limit}.',
+  'string.empty': message.empty,
+  'number.base': message.number,
+  'string.base': message.string,
+  'string.max': 'Deve ter no máximo {#limit} caracteres.'
 });
