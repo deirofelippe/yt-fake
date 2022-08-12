04c2f3e41214877a7b5eb146d5b5d74f892b91df
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-12 08:16:42 -0300

    CONFIG: backend - env.ts, adiciona novas propriedade e organiza

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -2,16 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  pagSeguroToken: process.env.PAG_SEGURO_TOKEN,
-  pagSeguroEmail: process.env.PAG_SEGURO_EMAIL,
-  pagSeguroUrlAuthSandbox: process.env.PAG_SEGURO_URL_AUTH_SANDBOX,
-  pagSeguroUrlRedirectSandbox: process.env.PAG_SEGURO_URL_CHECKOUT_SANDBOX,
+  pagSeguro: {
+    email: process.env.PAG_SEGURO_EMAIL,
+    sandbox: {
+      token: process.env.PAG_SEGURO_TOKEN,
+      authUrl: process.env.PAG_SEGURO_URL_AUTH_SANDBOX,
+      notificationConsultingUrl:
+        process.env.PAG_SEGURO_URL_SANDBOX_NOTIFICATION,
+      redirectUrl: process.env.PAG_SEGURO_URL_CHECKOUT_SANDBOX
+    }
+  },
   mercadoPagoIdApp: process.env.MERCADO_PAGO_ID_APP,
   mercadoPagoPublicKey: process.env.MERCADO_PAGO_PUBLIC_KEY,
   mercadoPagoAccessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
   ngrokToken: process.env.NGROK_TOKEN
 };
-
-//verifica se pode comprar,pega os valores, retorna a url
-//create playlist ja adiciona na library do channel
-//adicionar playlist de terceiro na propria library
