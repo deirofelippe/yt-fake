8b2694f0966d52d7c83bc87e09ab1ead957fdf8b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-12 06:27:16 -0300

    FEAT: backend - AddPlaylistToLibraryUsecase, cria a classe

---
+++ backend/src/domain/usecases/AddPlaylistToLibraryUsecase.ts
@@ -0,0 +1,42 @@
+import { ImpossibleActionError } from '../../errors/ImpossibleActionError';
+import { NotFoundError } from '../../errors/NotFoundError';
+import { PlaylistRepositoryInterface } from '../repositories/PlaylistRepositoryInterface';
+
+export type AddPlaylistToLibraryUsecaseDependencies = {
+  playlistRepository: PlaylistRepositoryInterface;
+};
+export type AddPlaylistToLibraryUsecaseInput = {
+  id_authenticated_channel: string;
+  id_playlist: string;
+};
+export type LibraryAttributes = { id_channel: string; id_playlist: string };
+
+export class AddPlaylistToLibraryUsecase {
+  constructor(private dependencies: AddPlaylistToLibraryUsecaseDependencies) {}
+  public async execute(input: AddPlaylistToLibraryUsecaseInput): Promise<void> {
+    const { playlistRepository } = this.dependencies;
+
+    const playlist = await playlistRepository.findById(input.id_playlist);
+    if (!playlist) throw new NotFoundError(input.id_playlist, 'Playlist');
+
+    if (playlist.channelIsTheSame(input.id_authenticated_channel))
+      throw new ImpossibleActionError(
+        'Não pode adicionar a própria playlist na própria library, ela já foi adicionada.'
+      );
+
+    if (playlist.isNotFree())
+      throw new ImpossibleActionError(
+        'Não pode adicionar playlist paga de terceiro na própria library.'
+      );
+
+    if (playlist.isPrivate())
+      throw new ImpossibleActionError(
+        'Não pode adicionar playlist private de terceiro na própria library.'
+      );
+
+    await playlistRepository.addToLibrary({
+      id_channel: input.id_authenticated_channel,
+      id_playlist: input.id_playlist
+    });
+  }
+}
