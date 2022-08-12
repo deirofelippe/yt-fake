ec45ad35f9cd6fa69ba3eab12362804f859466ee
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-12 08:04:15 -0300

    REFACTOR: backend - AddVideoToPlaylistUsecase, separa o metodo principal em pequenos metodos e muda nomes de variaveis

--- backend/src/domain/usecases/AddVideoToPlaylistUsecase.ts
+++ backend/src/domain/usecases/AddVideoToPlaylistUsecase.ts
@@ -1,5 +1,8 @@
 import { ImpossibleActionError } from '../../errors/ImpossibleActionError';
+import { NotAuthorizedError } from '../../errors/NotAuthorizedError';
 import { NotFoundError } from '../../errors/NotFoundError';
+import { Playlist } from '../entities/Playlist';
+import { Video } from '../entities/Video';
 import { IDGenerator } from '../libs/IDGenerator';
 import { PlaylistRepositoryInterface } from '../repositories/PlaylistRepositoryInterface';
 import { VideoRepositoryInterface } from '../repositories/VideoRepositoryInterface';
@@ -28,8 +31,7 @@ export class AddVideoToPlaylistUsecase {
   ) {}
 
   public async execute(input: AddVideoToPlaylistUsecaseInput) {
-    const { playlistRepository, videoRepository, idGenerator } =
-      this.dependencies;
+    const { playlistRepository, idGenerator } = this.dependencies;
 
     const videoInPlaylist: VideoInPlaylistAttributes = {
       id_playlist: input.id_playlist,
@@ -37,55 +39,67 @@ export class AddVideoToPlaylistUsecase {
       id: idGenerator.generate()
     };
 
-    const playlistFound = await playlistRepository.findById(
-      videoInPlaylist.id_playlist
+    const { playlist, video } = await this.findPlaylistAndVideo(
+      videoInPlaylist
     );
 
-    if (!playlistFound)
-      throw new NotFoundError(videoInPlaylist.id_playlist, 'Playlist');
-
-    const videoFound = await videoRepository.findById(
-      videoInPlaylist.id_playlist
-    );
-
-    if (!videoFound)
-      throw new NotFoundError(videoInPlaylist.id_playlist, 'Video');
-
-    const notOwnsThePlaylist = playlistFound.channelsIsNotTheSame(
+    const notOwnsThePlaylist = playlist.channelIsNotTheSame(
       input.id_authenticated_channel
     );
 
     if (notOwnsThePlaylist)
-      throw new ImpossibleActionError(
-        'Channel pode adicionar video somente na própria playlist.'
+      throw new NotAuthorizedError(
+        'Channel não pode adicionar video na própria playlist de terceiro.'
       );
 
-    let cantAddVideoToPlaylist = true;
+    const ownsTheVideo = video.channelIsTheSame(input.id_authenticated_channel);
+    const ownsVideoAndPlaylist = ownsTheVideo;
+    if (ownsVideoAndPlaylist) {
+      await playlistRepository.addVideo(videoInPlaylist);
+      return;
+    }
 
-    const playlistAndVideoOwnerIsNotSame = playlistFound.channelsIsNotTheSame(
-      videoFound.id_channel
+    this.ownsThePlaylistAndDoesntOwnTheVideo(playlist, video);
+
+    await playlistRepository.addVideo(videoInPlaylist);
+  }
+
+  private async findPlaylistAndVideo(
+    videoInPlaylist: VideoInPlaylistAttributes
+  ): Promise<never | { playlist: Playlist; video: Video }> {
+    const { playlistRepository, videoRepository } = this.dependencies;
+
+    const playlist = await playlistRepository.findById(
+      videoInPlaylist.id_playlist
     );
-    cantAddVideoToPlaylist =
-      playlistFound.isNotFree() && playlistAndVideoOwnerIsNotSame;
 
-    if (cantAddVideoToPlaylist)
+    if (!playlist)
+      throw new NotFoundError(videoInPlaylist.id_playlist, 'Playlist');
+
+    const video = await videoRepository.findById(videoInPlaylist.id_playlist);
+
+    if (!video) throw new NotFoundError(videoInPlaylist.id_playlist, 'Video');
+
+    return { playlist, video };
+  }
+
+  private ownsThePlaylistAndDoesntOwnTheVideo(
+    playlist: Playlist,
+    video: Video
+  ) {
+    if (playlist.isNotFree())
       throw new ImpossibleActionError(
         'Não pode adicionar video de terceiro na própria playlist paga.'
       );
 
-    const playlistIsFree = playlistFound.isFree();
-    cantAddVideoToPlaylist = playlistIsFree && videoFound.isNotFree();
-    if (cantAddVideoToPlaylist)
+    if (playlist.isFree() && video.isNotFree())
       throw new ImpossibleActionError(
         'Não pode adiciona video pago de terceiro na própria playlist.'
       );
 
-    cantAddVideoToPlaylist = playlistIsFree && videoFound.isPrivate();
-    if (cantAddVideoToPlaylist)
+    if (playlist.isFree() && video.isPrivate())
       throw new ImpossibleActionError(
         'Não pode adicionar video privado de terceiro na propria playlist.'
       );
-
-    await playlistRepository.addVideo(videoInPlaylist);
   }
 }
