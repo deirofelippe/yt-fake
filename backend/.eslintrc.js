2de070c24277a8d0935fb5a3dcbdada7f48dd1d4
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-16 20:38:19 -0300

    CONFIG: backend - eslint, tsconfig e makefile na raiz
    CONFIG: backend - eslint, tsconfig e makefile na raiz

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:security/recommended',
-    'prettier'
-  ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    sourceType: 'module',
-    ecmaVersion: 2019
-  },
   plugins: ['@typescript-eslint', 'sonarjs', 'jest'],
-  rules: {
-    'import/export': 'off'
-  }
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'plugin:sonarjs/recommended',
+        'plugin:security/recommended',
+        'prettier'
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+        ecmaVersion: 2019
+      },
+      rules: {
+        'import/export': 'off'
+      }
+    }
+  ]
 };
