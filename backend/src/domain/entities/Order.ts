fb4ac4b62f080eed80d7ae643d8c1893934b8eea
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-08-21 14:45:47 -0300

    FEAT: backend - entities/Order, adiciona atributos de pagamento e inicia valores default

--- backend/src/domain/entities/Order.ts
+++ backend/src/domain/entities/Order.ts
@@ -1,23 +1,43 @@
 import { CloneObject } from '../../utils/CloneObject';
 import { IDGenerator } from '../libs/IDGenerator';
 
+export enum PaymentGatewayTypes {
+  PAGSEGURO = 'pagseguro'
+}
+
 export type OrderAttributes = {
   id: string;
   id_channel: string;
   items: OrderItemAttributes[];
+  paymentGateway?: PaymentGatewayTypes;
+  transaction?: {
+    parcelas?: number;
+    status: string;
+    date?: string;
+  };
+  paymentMethod?: {
+    code: string;
+    type: string;
+  };
 };
 export type OrderItemAttributes = {
   id: string;
   id_order: string;
   id_purchased_item: string;
   type: 'video' | 'playlist';
+  amount?: number;
 };
 export type OrderDependencies = {
   idGenerator: IDGenerator;
 };
 
 export class Order {
-  private constructor(private attributes: OrderAttributes) {}
+  private constructor(private attributes: OrderAttributes) {
+    this.attributes = {
+      transaction: { status: 'Aguardando pagamento' },
+      ...attributes
+    };
+  }
 
   public static create(attributes: OrderAttributes) {
     return new Order(attributes);
